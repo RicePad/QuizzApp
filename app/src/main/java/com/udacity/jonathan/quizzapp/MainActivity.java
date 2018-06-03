29218f781dcb5006063ab77c81fd420349a9e07b
JON <jonn.thinkrtc@gmail.com>
2018-06-03 15:33:01 -0400

    Add helper functions on MainActivity.java

--- app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button submit;
-    int rightAnswers = 0;
+    Button submitScore;
+    int totalRghtAnswers = 0;
 
 
 
@@ -23,10 +23,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        submit = (Button) findViewById(R.id.submitScore);
+        submitScore = (Button) findViewById(R.id.submitScore);
 
     }
 
 
+    private void verifyQ1Answer(){
+        RadioButton radioButton5Years = (RadioButton) findViewById(R.id.radio_button_5_years);
+        boolean radioButton5YearsSelected = radioButton5Years.isChecked();
+        if(radioButton5YearsSelected){
+            totalRghtAnswers += 1;
+        }
+    }
 
+
+
+    private void verifyQ2Answer(){
+        RadioButton radioButton50States = (RadioButton) findViewById(R.id.radio_button_50_states);
+        boolean radioButton50States = radioButton50States.isChecked();
+        if(radioButton50States){
+            totalRghtAnswers += 1;
+        }
+    }
+
+    private String Q3EditTextInput(){
+        EditText checkUserPresidentInput = (EditText) findViewById(R.id.edit_text_first_president);
+        String presidentFullName = checkUserPresidentInput.getText().toString();
+        return presidentFullName;
+    }
+
+    private void verifyQ3EditTextInput(){
+        String fullName = Q3EditTextInput();
+        if(fullName.trim().equalsIgnoreCase("George Washington")){
+            totalRghtAnswers += 1;
+        }
+    }
 }
