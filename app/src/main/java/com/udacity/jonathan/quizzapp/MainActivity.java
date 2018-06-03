5e4522b605e33edc6213460a804d6e6ec67236b6
JON <jonn.thinkrtc@gmail.com>
2018-06-02 22:26:49 -0400

    Finished design layout and added initial logic

--- app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.udacity.jonathan.quizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submit;
+    int rightAnswers = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        submit = (Button) findViewById(R.id.submitScore);
+
     }
+
+
+
 }
