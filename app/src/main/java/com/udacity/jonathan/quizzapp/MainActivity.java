183a9e9817b3b4a204b1ebd7d690fd68e1b14088
JON <jonn.thinkrtc@gmail.com>
2018-06-04 23:07:01 -0400

    Add helper functions for Q4 and EventListeners on MainActivity.java

--- app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/quizzapp/MainActivity.java
@@ -12,11 +12,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button submitScore;
-    int totalRghtAnswers = 0;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +19,41 @@ public class MainActivity extends AppCompatActivity {
 
 
         submitScore = (Button) findViewById(R.id.submitScore);
+        submitScore.setOnClickListener( submitScoreButton);
 
     }
 
+
+    Button submitScore;
+    int totalRightAnswers = 0;
+
+
+
+    private void verifyQuestions(){
+        verifyQ1Answer();
+        verifyQ2Answer();
+        verifyQ3EditTextInput();
+        verifyQ4Answer();
+        verifyQ5EditTextInput();
+    }
+
+    private void resetScoreboard(){
+        totalRightAnswers = 0;
+    }
+
+    final View.OnClickListener submitScoreButton = new View.OnClickListener() {
+      public void onClick(final View v){
+          verifyQuestions();
+          Toast.makeText(MainActivity.this, "Right Answers: " + totalRightAnswers + "/5", Toast.LENGTH_LONG).show();
+          resetScoreboard();
+      }
+    };
 
     private void verifyQ1Answer(){
         RadioButton radioButton5Years = (RadioButton) findViewById(R.id.radio_button_5_years);
         boolean radioButton5YearsSelected = radioButton5Years.isChecked();
         if(radioButton5YearsSelected){
-            totalRghtAnswers += 1;
+            totalRightAnswers += 1;
         }
     }
 
@@ -40,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void verifyQ2Answer(){
         RadioButton radioButton50States = (RadioButton) findViewById(R.id.radio_button_50_states);
-        boolean radioButton50States = radioButton50States.isChecked();
-        if(radioButton50States){
-            totalRghtAnswers += 1;
+        boolean radioButton50StatesSelected  = radioButton50States.isChecked();
+        if(radioButton50StatesSelected){
+            totalRightAnswers += 1;
         }
     }
 
@@ -55,9 +76,24 @@ public class MainActivity extends AppCompatActivity {
     private void verifyQ3EditTextInput(){
         String fullName = Q3EditTextInput();
         if(fullName.trim().equalsIgnoreCase("George Washington")){
-            totalRghtAnswers += 1;
+            totalRightAnswers += 1;
         }
     }
+
+    private void verifyQ4Answer(){
+        CheckBox freedomSpeech = (CheckBox) findViewById(R.id.freedom_speech);
+        CheckBox freedomPet = (CheckBox) findViewById(R.id.freedom_pet);
+        CheckBox freedomReligion = (CheckBox) findViewById(R.id.freedom_religion);
+
+        boolean freedomSpeechChecked = freedomSpeech.isChecked();
+        boolean freedomPetChecked = freedomPet.isChecked();
+        boolean freedomReligionChecked = freedomReligion.isChecked();
+
+        if(freedomSpeechChecked && !freedomPetChecked && freedomReligionChecked){
+            totalRightAnswers += 1;
+        }
+    }
+
 
     private String Q5EditTextInput(){
         EditText check44thPresidentNameInput = (EditText) findViewById(R.id.edit_text_44th_president);
@@ -68,7 +104,10 @@ public class MainActivity extends AppCompatActivity {
     private void verifyQ5EditTextInput(){
         String fullName = Q5EditTextInput();
         if(fullName.trim().equalsIgnoreCase("Barack Obama")){
-            totalRghtAnswers += 1;
+            totalRightAnswers += 1;
         }
     }
+
+
+
 }
